6ffa594336a7d6bf61af831227bbaa4ec16a23f3
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-03-16 09:41:36 +0100

    v1.1.1

--- lib/generate_image.rb
+++ lib/generate_image.rb
@@ -1,56 +1,36 @@
 require 'net/http'
+require 'json'
 
 module GenerateImage
-  class RequestFailed < StandardError
-    def initialize(message = "Request failed")
-      super(message)
-    end
-  end
+  class RequestFailed < StandardError; end
 
-  class MissingApiKey < RequestFailed
-    def initialize(message = "API key not set")
-      super(message)
-    end
-  end
-
-  class << self
+  class Client
     API_ENDPOINT = 'https://api.openai.com/v1/images/generations'
-    API_KEY = ENV['DALL_E_API_KEY']
-    DEFAULT_OPTIONS = {
-      model: 'image-alpha-001',
-      num_images: 1,
-      size: '1024x1024',
-      response_format: 'url',
-      style: nil,
-      scale: 1,
-      seed: nil,
-      quality: 100,
-      text_model: 'text-davinci-002',
-      text_prompt: nil,
-      text_length: nil
-    }
+    IMAGE_MODEL_NAME = 'image-alpha-001'
+    TEXT_MODEL_NAME = 'text-davinci-002'
+
+    attr_accessor :api_key
+
+    def initialize(api_key = nil)
+      @api_key = api_key || ENV['DALL_E_API_KEY']
+      raise StandardError, "API Key not set" unless @api_key
+    end
 
     def generate_image(text, options = {})
-      options = DEFAULT_OPTIONS.merge(options)
-      raise MissingApiKey if API_KEY.nil?
-
       uri = URI(API_ENDPOINT)
       request = Net::HTTP::Post.new(uri)
       request['Content-Type'] = 'application/json'
-      request['Authorization'] = "Bearer #{API_KEY}"
+      request['Authorization'] = "Bearer #{@api_key}"
       request.body = {
-        model: options[:model],
+        model: options[:model] || IMAGE_MODEL_NAME,
         prompt: text,
-        num_images: options[:num_images],
-        size: options[:size],
-        response_format: options[:response_format],
-        style: options[:style],
-        scale: options[:scale],
-        seed: options[:seed],
-        quality: options[:quality],
-        text_model: options[:text_model],
-        text_prompt: options[:text_prompt],
-        text_length: options[:text_length]
+        num_images: options[:num_images] || 1,
+        size: options[:size] || '512x512',
+        response_format: options[:response_format] || 'url',
+        style: options[:style] || nil,
+        scale: options[:scale] || 1,
+        seed: options[:seed] || nil,
+        quality: options[:quality] || 80
       }.to_json
 
       response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
@@ -60,14 +40,15 @@ module GenerateImage
       if response.code == '200'
         if options[:response_format] == 'base64'
           image_base64 = JSON.parse(response.body)['data'][0]['base64']
-          { image_base64: image_base64 }
+          return { image_base64: image_base64 }
         else
           image_url = JSON.parse(response.body)['data'][0]['url']
-          { image_url: image_url }
+          return { image_url: image_url }
         end
       else
-        raise RequestFailed.new("Failed to generate image. Response code: #{response.code}. Response body: #{response.body}")
+        raise RequestFailed, "Failed to generate image. Response code: #{response.code}. Response body: #{response.body}"
       end
     end
+
   end
 end
